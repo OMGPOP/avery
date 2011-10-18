47cc484fbd97e3b1621a64865962c9a51f958c3f
christopher holt <chris@omgpop.com>
2011-10-17 23:57:25 -0400

    comma ON

--- public/avery.js
+++ public/avery.js
@@ -2,7 +2,7 @@ var Avery = {
   watch: function($element, range, offset, metrics) {
     $.getJSON("/fetch", { range: range, offset: offset, metrics: metrics }, function(x) {
       var stacked = metrics.length == 1 ? x['metrics'][0]['values'] : _.map(_.zip.apply([], _.pluck(x['metrics'], 'values')), function(c) { return _.reduce(c, function(d,e) { return Number(d)+Number(e) }) })
-      $element.sparkline(stacked, { height:'100px', width: '200px', type: 'line', fillColor: '#DDDDDD', lineColor: '#03ACE3', lineWidth: 2, chartRangeMin: 0, chartRangeMax: 5 })
+      $element.sparkline(stacked, { height:'150px', width: '300px', type: 'line', fillColor: '#DDDDDD', lineColor: '#03ACE3', lineWidth: 2, chartRangeMin: 0, chartRangeMax: 5 })
     })
   }
 }
