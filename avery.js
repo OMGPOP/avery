c0dc324a7e0ecd972cec537c578aaba6842bc7ca
christopher holt <cwholt@gmail.com>
2011-12-12 12:36:15 -0500

    debug

--- avery.js
+++ avery.js
@@ -127,7 +127,6 @@ app.get("/fetch", function(req, res) {
   res.header('Access-Control-Allow-Origin','*');
   var result = { success: true, ts: time, metrics: [] };
   function getMetrics(x) {
-    console.log("METRICS: ",metrics.length);
     if (x < metrics.length) {
       var metric = metrics[x];
       if (metric.key == "all") {
@@ -151,7 +150,9 @@ app.get("/fetch", function(req, res) {
         })
       }
     } else {
-      console.log("RESULT: ",result['metrics'].length);
+      result['metrics'].forEach(function(metric) {
+        console.log(_.pluck(metric, 'values'));
+      })
       result['metrics'] = result['metrics'].length == 1 ? result['metrics'] : [ { metric: 'all/'+metrics[0]['metric'], values: _.compact(_.map(_.zip.apply([], _.pluck(result['metrics'], 'values')), function(c) { return _.reduce(c, function(d,e) { return Number(d||0)+Number(e||0) }) })) } ];
       res.send(result)
     }
